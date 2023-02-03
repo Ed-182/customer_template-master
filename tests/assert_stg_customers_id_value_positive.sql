1a0cd90dedcb41fe73ca1abf0d4f32ab6c954fdd
Edgar Duarte <e.josue.duarte@gmail.com>
2023-02-02 23:57:34 -0600

    singular testing

--- tests/assert_stg_customers_id_value_positive.sql
+++ tests/assert_stg_customers_id_value_positive.sql
@@ -1,11 +1,12 @@
-id > 0
-with
-customers as (
- select * from {{ ref('stg_customers') }}
+with customers as (
+    select * from {{ ref('stg_customers') }}
 )
 
-select
- id
-from
- customers
-where
+select 
+    customer_id,
+    sum(customer_id) as total_customer_id
+
+from  customers
+
+group by 1
+having total_customer_id < 0
