9eea241bc708dbdd24301ba222f89efcd94b6228
Edgar Duarte <e.josue.duarte@gmail.com>
2023-02-03 21:52:01 -0600

    macroing

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -5,7 +5,7 @@ select
     status,
 
     -- amount is stored in cents, convert it to dollars
-    {{ cents_to_dollars('amount', 6) }} as amount,
+    {{ cents_to_dollars('amount', 0) }} as amount,
     created as created_at
 
 from `dbt-tutorial`.stripe.payment
