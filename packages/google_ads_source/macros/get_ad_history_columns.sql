e901bf230643621e0ddf4015037e7b7b1f635b1d
Edgar Duarte <ed.albarn@gmail.com>
2023-02-07 15:30:30 -0600

    installed fivetran/add_reporting package

---
+++ packages/google_ads_source/macros/get_ad_history_columns.sql
@@ -0,0 +1,16 @@
+{% macro get_ad_history_columns() %}
+
+{% set columns = [
+    {"name": "ad_group_id", "datatype": dbt.type_int()},
+    {"name": "display_url", "datatype": dbt.type_string()},
+    {"name": "final_urls", "datatype": dbt.type_string()},
+    {"name": "id", "datatype": dbt.type_int()},
+    {"name": "name", "datatype": dbt.type_string()},
+    {"name": "status", "datatype": dbt.type_string()},
+    {"name": "type", "datatype": dbt.type_string()},
+    {"name": "updated_at", "datatype": dbt.type_timestamp()}
+] %}
+
+{{ return(columns) }}
+
+{% endmacro %}
---
+++ packages/pinterest_source/macros/get_ad_group_history_columns.sql
@@ -0,0 +1,16 @@
+{% macro get_ad_group_history_columns() %}
+
+{% set columns = [
+    {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
+    {"name": "campaign_id", "datatype": dbt.type_string()},
+    {"name": "created_time", "datatype": dbt.type_timestamp()},
+    {"name": "end_time", "datatype": dbt.type_timestamp()},
+    {"name": "id", "datatype": dbt.type_string()},
+    {"name": "name", "datatype": dbt.type_string()},
+    {"name": "start_time", "datatype": dbt.type_timestamp()},
+    {"name": "status", "datatype": dbt.type_string()}
+] %}
+
+{{ return(columns) }}
+
+{% endmacro %}
